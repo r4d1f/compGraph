d151b72ae7d3213c40663463f58e2f7746894c9d
Айназаров Радиф Рамисович <Aynazarov_RR@local.mirea.ru>
2020-12-23 10:09:24 +0300

    Кг

---
+++ OpenGL_Start/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace kg_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(406, 352);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 352);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OpenGL_Start/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+// для работы с библиотекой OpenGL 
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT 
+using Tao.FreeGlut;
+
+namespace kg_1
+{
+    public partial class Form1 : Form
+    {
+        private float angleX = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDevice();
+
+        }
+
+        private void InitializeDevice()
+        {
+            simpleOpenGlControl1.InitializeContexts();
+
+            // инициализация Glut 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+            // очитка окна 
+            Gl.glClearColor(1, 1, 1, 1.0f);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            // установка порта вывода в соотвествии с размерами элемента simpleOpenGlControl1 
+            Gl.glViewport(0, 0, simpleOpenGlControl1.Width, simpleOpenGlControl1.Height);
+        }
+
+
+
+        private void VertexDeclaration()
+        {
+            Gl.glBegin(Gl.GL_POLYGON);
+            Color[] color = { Color.Yellow, Color.Red, Color.Blue, Color.Green};
+            Gl.glColor3d((double)color[0].R / 256, (double)color[0].G / 256, (double)color[0].B / 256); Gl.glVertex3d(4, 4, 0); Gl.glNormal3d(0, 0, 1);
+            Gl.glColor3d((double)color[1].R / 256, (double)color[1].G / 256, (double)color[1].B / 256); Gl.glVertex3d(-4, 4, 0); Gl.glNormal3d(0, 0, 1);
+            Gl.glColor3d((double)color[2].R / 256, (double)color[2].G / 256, (double)color[2].B / 256); Gl.glVertex3d(-4, -4, 0); Gl.glNormal3d(0, 0, 1);
+            Gl.glColor3d((double)color[3].R / 256, (double)color[3].G / 256, (double)color[3].B / 256); Gl.glVertex3d(4, -4, 0); Gl.glNormal3d(0, 0, 1);
+            Gl.glEnd();
+        }
+
+        private void SetCamera()
+        {
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(80, (float)simpleOpenGlControl1.Width / (float)simpleOpenGlControl1.Height, 1, 80);
+
+            //видовое преобразование - установка камеры
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            Glu.gluLookAt(20, 20, 20, 0, 0, 0, 0, 1, 0);
+            // настройка параметров OpenGL для визуализации 
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+            //сглаживание
+            Gl.glShadeModel(Gl.GL_SMOOTH);
+
+            //установка света
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+            Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, new float[] {1,1,1, 0f });
+
+            //установка материала 
+            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+
+
+
+        }
+
+        private void MoveCamera()
+        {
+            Glu.gluLookAt(20, 20, 20, 0, 0, 0, 0, 1, 0);
+            Gl.glRotatef(angleX, 1, 0, 0);
+            angleX += 2;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetCamera();
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //функция выполняется один раз - как только отрисовывается окно
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1.0f, 0, 0);
+
+            //преобразование - трансляция, поворот и масштабирование
+            Gl.glPushMatrix();
+
+            VertexDeclaration();
+
+            Gl.glPopMatrix();
+
+
+            // завершаем режим рисования примитивов 
+            Gl.glEnd();
+
+            // дожидаемся завершения визуализации кадра 
+            Gl.glFlush();
+
+            //перерисовка
+            simpleOpenGlControl1.Invalidate();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //таймер вызывает событие Tick раз в timer1.Interval миллисекунд - используем для перерисовки
+            timer1.Stop();
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1.0f, 0, 0);
+
+            Gl.glFlush();
+
+            //движение камеры и переобъявление вершин
+            MoveCamera();
+            VertexDeclaration();
+
+
+            Gl.glFlush();
+            simpleOpenGlControl1.Invalidate();
+            timer1.Start();
+        }
+    }
+}
